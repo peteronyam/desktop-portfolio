ecb2bbba5b1ece2443a1d879c3adfb25e6e2149b
abenezer-addisu <dasesatube@gmail.com>
2023-04-21 11:22:07 +0300

    Update javascript file

--- main.js
+++ main.js
@@ -2,7 +2,15 @@ const menuButton = document.querySelector(".mobile_menu");
 const mobileMenu = document.querySelector("#menu_top");
 const closeButton = document.querySelector("#close-button");
 
-
+mobileMenu.style.display = "none";
+menuButton.addEventListener("click", openMobileMenu);
+closeButton.addEventListener("click", closeMobileMenu);
+function openMobileMenu() {
+  mobileMenu.style.display = "block";
+}
+function closeMobileMenu() {
+  mobileMenu.style.display = "none";
+}
 
 const menuItems = document.querySelectorAll("#menu_top a");
 
