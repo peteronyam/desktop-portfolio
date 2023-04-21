ff0a20cbd1aa89f46b9e64133ef57385e5d19a48
PETER ONYAM <peteronyam@gmail.com>
2023-04-21 09:16:17 +0100

    update mobile

--- main.js
+++ main.js
@@ -2,15 +2,16 @@ const menuButton = document.querySelector(".mobile_menu");
 const mobileMenu = document.querySelector("#menu_top");
 const closeButton = document.querySelector("#close-button");
 
-mobileMenu.style.display = "none";
 
-menuButton.addEventListener("click", openMobileMenu);
-closeButton.addEventListener("click", closeMobileMenu);
 
-function openMobileMenu() {
-  mobileMenu.style.display = "block";
-}
+const menuItems = document.querySelectorAll("#menu_top a");
 
-function closeMobileMenu() {
-  mobileMenu.style.display = "none";
-}
+menuItems.forEach(function(item) {
+  item.addEventListener("click", function() {
+    closeMobileMenu();
+    const target = this.getAttribute("href");
+    document.querySelector(target).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
