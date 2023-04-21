e5ae69e67a8f4490a9331dd54d1d217128fa4a8f
PETER ONYAM <peteronyam@gmail.com>
2023-04-21 09:50:36 +0100

    update main.js

--- main.js
+++ main.js
@@ -1,25 +1,26 @@
-const menuButton = document.querySelector(".mobile_menu");
-const mobileMenu = document.querySelector("#menu_top");
-const closeButton = document.querySelector("#close-button");
+const menuButton = document.querySelector('.mobile_menu');
+const mobileMenu = document.querySelector('#menu_top');
+const closeButton = document.querySelector('#close-button');
 
-mobileMenu.style.display = "none";
-menuButton.addEventListener("click", openMobileMenu);
-closeButton.addEventListener("click", closeMobileMenu);
 function openMobileMenu() {
-  mobileMenu.style.display = "block";
+  mobileMenu.style.display = 'block';
 }
 function closeMobileMenu() {
-  mobileMenu.style.display = "none";
+  mobileMenu.style.display = 'none';
 }
+mobileMenu.style.display = 'none';
+menuButton.addEventListener('click', openMobileMenu);
+closeButton.addEventListener('click', closeMobileMenu);
 
-const menuItems = document.querySelectorAll("#menu_top a");
 
-menuItems.forEach(function(item) {
-  item.addEventListener("click", function() {
+const menuItems = document.querySelectorAll('#menu_top a');
+
+menuItems.forEach((item) => {
+  item.addEventListener('click', function() {
     closeMobileMenu();
-    const target = this.getAttribute("href");
+    const target = this.getAttribute('href');
     document.querySelector(target).scrollIntoView({
-      behavior: "smooth"
+      behavior: 'smooth'
     });
   });
 });
